71ede6d17f501afade07314dc97f8aa05026ba20
duybui1311 <117589301+duybui1311@users.noreply.github.com>
2024-04-06 22:37:20 +0700

    fixed some minor issue

--- Asm1 further prog/InsuranceClaimsManagementSystem/src/InsuranceClaimManagementSystem/util/DataGenerator.java
+++ Asm1 further prog/InsuranceClaimsManagementSystem/src/InsuranceClaimManagementSystem/util/DataGenerator.java
@@ -223,7 +223,7 @@ public class DataGenerator {
     public class NameGenerator {
         private static final Random random = new Random();
 
-        private static final String[] firstNames = {"John", "Emma", "Michael", "David", "William", "Mai", "Nghia", "Son", "Hai", "Duy", "Tri", "Ha", "Phanh", "Hoe","Hai Minh","Thao"};
+        private static final String[] firstNames = {"John", "Emma", "Michael", "David","Ngoc Ha", "Mai", "Nghia", "Son", "Hai", "Duy", "Tri", "Ha", "Phanh", "Hoe","Hai Minh","Thao"};
         private static final String[] lastNames = {"Smith", "Johnson", "Williams", "Jones", "Sins", "Tran", "Miller", "Wilson", "Moore", "Taylor","Nguyen", "Tran","Bui"};
 
         public static String generateFullName() {
